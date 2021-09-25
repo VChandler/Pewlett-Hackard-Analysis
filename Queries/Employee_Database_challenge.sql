1e88896148772fa4b17263ac0cd0281fa84399c7
Vince Chandler <vincechandler@gmail.com>
2021-09-25 14:52:33 -0500

    adding deliverable 2 files

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,4 +1,5 @@
 -- DELIVERABLE 1
+-- Retirement Titles table for employees born between 1/1/52 and 12/31/55
 SELECT e.emp_no,
 	e.first_name,
 	e.last_name,
@@ -30,3 +31,26 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+-- Deliverable 2
+-- Mentorship eligibility
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE t.to_date = '9999-01-01' AND 
+	de.to_date = '9999-01-01' AND
+	e.birth_date >= '1965-01-01' AND
+	e.birth_date <= '1965-12-31'
+ORDER BY e.emp_no;
+
+select * from titles;
